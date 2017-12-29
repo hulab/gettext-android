e23b902fccc3fc64c8d7dc74dd8a55498a0fb4eb
Pierre-Emmanuel Altieri <pe.altieri@gmail.com>
2017-12-29 11:28:25 +0100

    Added public documentation on PO.java

--- gettext/src/main/java/com/hulab/gettext/PO.java
+++ gettext/src/main/java/com/hulab/gettext/PO.java
@@ -1,21 +1,7 @@
-/*
- * Copyright 2014 Aurélien Gâteau <mail@agateau.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.hulab.gettext;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import java.util.Locale;
 
@@ -34,7 +20,14 @@ public class PO {
         return sMessages;
     }
 
-    public static void setLocale(Context context, String locale) {
+    /**
+     * Set the current app's language manually. Otherwise, it will just take into account the
+     * default locale.
+     *
+     * @param context The current context. This context must be non-null.
+     * @param locale  The ISO code of the language, for example "en", "ru", "fr", "fr_CH".
+     */
+    public static void setLocale(@NonNull Context context, String locale) {
 
         try {
 
@@ -53,13 +46,45 @@ public class PO {
         }
     }
 
-    public static String gettext(Context context, String msgid) {
+    /**
+     * Returns the translated string for the given key. The string is translated in the set language
+     * if such a file is available.
+     * <p>
+     * Fallbacks are the following if only "en", "fr", and "pt-BR" are available:
+     * <p>
+     * "en_US" -> "en"
+     * "fr_CH" -> "fr"
+     * "pt_BR" -> "pt_BR"
+     * "pt" -> {@param msgid}
+     *
+     * @param context the non-null context
+     * @param msgid   the key of the message
+     * @return The translated message for the given key.
+     */
+    public static String gettext(@NonNull Context context, String msgid) {
         Messages messages = sharedMessages(context);
         if (messages != null)
             return messages.gettext(msgid);
         return msgid;
     }
 
+    /**
+     * Returns the translated string for the given key. The string is translated in the set language
+     * if such a file is available.
+     * <p>
+     * Fallbacks are operating in the same way than {@link #gettext(Context, String)}.
+     * Plurals rules are referring to the ones given in the .po files. If no locale is available,
+     * The last fallback is "en" rules: if {@param n} == 1, we use {@param msgid} as fallback. If
+     * different than 1, we use {@param msgid_plural} as fallback.
+     *
+     * @param context      the non-null context
+     * @param msgid        the key of the message
+     * @param msgid_plural the plural key of the message
+     * @param n            the quantity to consider when getting the plural form
+     * @return The translated message for the given key in the given quantity.
+     * Quantity is injected in the resulting string using
+     * {@link java.lang.String#format(String, Object...) String.format()} method.
+     */
     public static String ngettext(Context context, String msgid, String msgid_plural, int n) {
         Messages messages = sharedMessages(context);
         if (messages != null)
